087d01b5bdd57f5b741bc72efd4457d8627bfdb7
danielforkner <daniel.forkner@gmail.com>
2023-02-10 08:33:47 -0500

    users router

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Mount the router at the /users path:
+app.use('/users', require('./api/usersRouter'));
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
